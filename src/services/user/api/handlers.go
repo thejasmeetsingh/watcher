fa22a4cdf43b1d7a436235c49a392543fc598f9c
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2024-08-30 21:36:15 +0530

    Added request params in handlers

--- src/services/user/api/handlers.go
+++ src/services/user/api/handlers.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/gofiber/fiber/v2"
+	log "github.com/sirupsen/logrus"
 )
 
 func HealthCheck(ctx *fiber.Ctx) error {
@@ -11,24 +12,64 @@ func HealthCheck(ctx *fiber.Ctx) error {
 }
 
 func (apiCfg *APIConfig) Signup(ctx *fiber.Ctx) error {
+	ctx.Accepts("application/json")
+
+	var params credentials
+
+	if err := ctx.BodyParser(params); err != nil {
+		return err
+	}
+
+	log.Infoln(params)
+
 	return ctx.JSON(fiber.Map{
 		"message": "Create user account",
 	})
 }
 
 func (apiCfg *APIConfig) Login(ctx *fiber.Ctx) error {
+	ctx.Accepts("application/json")
+
+	var params credentials
+
+	if err := ctx.BodyParser(params); err != nil {
+		return err
+	}
+
+	log.Infoln(params)
+
 	return ctx.JSON(fiber.Map{
 		"message": "Login the user",
 	})
 }
 
 func (apiCfg *APIConfig) UpdateProfile(ctx *fiber.Ctx) error {
+	ctx.Accepts("application/json")
+
+	var params profile
+
+	if err := ctx.BodyParser(params); err != nil {
+		return err
+	}
+
+	log.Infoln(params)
+
 	return ctx.JSON(fiber.Map{
 		"message": "Update user profile",
 	})
 }
 
 func (apiCfg *APIConfig) UpdatePassword(ctx *fiber.Ctx) error {
+	ctx.Accepts("application/json")
+
+	var params password
+
+	if err := ctx.BodyParser(params); err != nil {
+		return err
+	}
+
+	log.Infoln(params)
+
 	return ctx.JSON(fiber.Map{
 		"message": "Update user password",
 	})
