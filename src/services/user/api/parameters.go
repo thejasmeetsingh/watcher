f1689b46d6cf62b91258eae80f5a04d5990bf6d5
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-08-31 17:33:46 +0530

    Updated request params schemas and added DB queries

--- src/services/user/api/parameters.go
+++ src/services/user/api/parameters.go
@@ -1,18 +1,24 @@
 package api
 
-type credentials struct {
+type Signup struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+	Name     string `json:"name" validate:"required,max=50"`
+}
+
+type Login struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=8"`
 }
 
-type profile struct {
+type Profile struct {
 	Email  string   `json:"email" validate:"email"`
-	Name   string   `json:"name" validate:max=50`
-	Age    int      `json:"age" validate:min=12`
-	Gender string   `json:"gender" validate:min=1`
+	Name   string   `json:"name" validate:"max=50"`
+	Age    int32    `json:"age" validate:"min=12"`
+	Gender string   `json:"gender" validate:"min=1"`
 	Genres []string `json:"genres"`
 }
 
-type password struct {
+type Password struct {
 	Password string `json:"password" validate:"required,min=8"`
 }
