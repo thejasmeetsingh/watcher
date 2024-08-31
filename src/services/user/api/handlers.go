bb62dd7b6527653c56842fe889ec102619cacdc8
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-08-31 17:34:03 +0530

    Updated singup API

--- src/services/user/api/handlers.go
+++ src/services/user/api/handlers.go
@@ -3,7 +3,21 @@ package api
 import (
 	"github.com/gofiber/fiber/v2"
 	log "github.com/sirupsen/logrus"
+	"github.com/thejasmeetsingh/watcher/src/services/user/database"
 )
+
+func getUserProfileData(user *database.User) fiber.Map {
+	return fiber.Map{
+		"id":          user.ID.String(),
+		"email":       user.Email,
+		"name":        user.Name,
+		"age":         user.Age,
+		"gender":      string(user.Gender.GenderType),
+		"genres":      user.Genres,
+		"created_at":  user.CreatedAt.Time,
+		"modified_at": user.ModifiedAt.Time,
+	}
+}
 
 func HealthCheck(ctx *fiber.Ctx) error {
 	return ctx.JSON(fiber.Map{
@@ -14,23 +28,39 @@ func HealthCheck(ctx *fiber.Ctx) error {
 func (apiCfg *APIConfig) Signup(ctx *fiber.Ctx) error {
 	ctx.Accepts("application/json")
 
-	var params credentials
+	params := new(Signup)
 
 	if err := ctx.BodyParser(params); err != nil {
 		return err
 	}
 
-	log.Infoln(params)
+	// Check if user with the given email ID already exists
+	_, err := getUserByEmail(apiCfg, ctx.Context(), params.Email)
+	if err == nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Email already exists",
+		})
+	}
 
-	return ctx.JSON(fiber.Map{
+	// Create user in DB
+	user, err := createUser(apiCfg, ctx.Context(), *params)
+	if err != nil {
+		log.Errorln(err)
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Something went wrong. Please try after some time",
+		})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"message": "Create user account",
+		"data":    getUserProfileData(user),
 	})
 }
 
 func (apiCfg *APIConfig) Login(ctx *fiber.Ctx) error {
 	ctx.Accepts("application/json")
 
-	var params credentials
+	var params Login
 
 	if err := ctx.BodyParser(params); err != nil {
 		return err
@@ -46,7 +76,7 @@ func (apiCfg *APIConfig) Login(ctx *fiber.Ctx) error {
 func (apiCfg *APIConfig) UpdateProfile(ctx *fiber.Ctx) error {
 	ctx.Accepts("application/json")
 
-	var params profile
+	var params Profile
 
 	if err := ctx.BodyParser(params); err != nil {
 		return err
@@ -62,7 +92,7 @@ func (apiCfg *APIConfig) UpdateProfile(ctx *fiber.Ctx) error {
 func (apiCfg *APIConfig) UpdatePassword(ctx *fiber.Ctx) error {
 	ctx.Accepts("application/json")
 
-	var params password
+	var params Password
 
 	if err := ctx.BodyParser(params); err != nil {
 		return err
