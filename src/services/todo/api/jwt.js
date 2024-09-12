ff8f41a9fd6335ef13e8ca66aefb45116508a041
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-09-12 21:09:36 +0530

    Added jwt module to verify user auth token

---
+++ src/services/todo/api/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const getSecretKey = () => {
+  let secretKey = process.env.SECRET_KEY;
+
+  if (secretKey === "") {
+    secretKey = "random-secret-123";
+  }
+
+  return secretKey;
+};
+
+const verifyToken = (token) => {
+  const secretKey = getSecretKey();
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    return { valid: true, decoded };
+  } catch (error) {
+    return { valid: false, error: error.message };
+  }
+};
+
+module.exports = verifyToken;
